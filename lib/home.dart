ccd82ada824ffbbbca56b37a0baf0ef716752111
Justin Navarro <info@justinnavarro.net>
2019-04-17 17:22:04 -0400

    set up the app bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+	final topBar = AppBar(
+		backgroundColor: Color(0xfff8faf8),
+		centerTitle: true,
+		elevation: 1.0,
+		leading: Icon(Icons.camera_alt),
+		title: SizedBox(
+			height: 35.0,
+			child: Image.asset("assets/images/insta_logo.png"),
+		),
+		actions: <Widget>[
+			Padding(
+				padding: const EdgeInsets.only(right: 12.0),
+				child: Icon(Icons.send),
+			)
+		],
+	);
+
 	@override
 	Widget build(BuildContext context) {
-    	return Container(
-			margin: EdgeInsets.all(30.0),
-			child: Text('Just like that'),
+    	return Scaffold(
+			appBar: topBar,
 		);
   	}
 }
